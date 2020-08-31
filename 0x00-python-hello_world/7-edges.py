87e097a716aa54a4040e4c44e75fcdac913a9c53
thiago <thiago@kali>
2020-08-31 14:19:29 -0500

    [update]--file--

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word_last_2 = middle_word = word
+print("First 3 letters: {}".format(word_first_3[:3]))
+print("Last 2 letters: {}".format(word_last_2[-2:]))
+print("Middle word: {}".format(middle_word[1:-1]))
