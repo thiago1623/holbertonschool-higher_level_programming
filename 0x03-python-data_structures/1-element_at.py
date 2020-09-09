ebd392d68fb5f75c9b99d185fea4c1a577f923c8
thiago <thiago@kali>
2020-09-08 20:37:49 -0500

    [update]--FILE--

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-
 def element_at(my_list, idx):
-    if idx < 0 and idx > len(my_list):
-        return None
-    else:
+    if idx < len(my_list) and idx >= 0:
         return my_list[idx]
+    elif idx < 0 and idx > len(my_list):
+        return None
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def print_matrix_integer(matrix=[[]]):
-    for item in matrix:
-        print(' '.join("{}".format(j)for j in item))
+    for i in matrix:
+        print(' '.join("{:d}".format(j)for j in i))
