4a3629fc283e0194a40cca9e8b0ef43430cd3d9f
thiago <thiago@kali>
2020-09-21 13:30:30 -0500

    [update]--TESTS--FILE--

---
+++ 0x05-python-exceptions/tests/3-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+
+if __name__ == "__main__":
+    sys.path.append("..")
+    safe_print_division = __import__('3-safe_print_division').safe_print_division
+
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
+
+    a = 12
+    b = 0
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
