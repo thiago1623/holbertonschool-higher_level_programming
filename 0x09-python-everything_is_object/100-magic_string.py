48bac37f75ae3e188d841d20098eece7556930aa
thiago1623 <1636@holbertonschool.com>
2020-09-29 18:29:06 -0500

    [update]--FILE--

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyton3
-def magic_string(l = []):
-    l.append("Holberton")
-    return ", ".join(l)
+#!/usr/bin/python3
+def magic_string():
+    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
+    return ", ".join(["Holberton" for i in range(magic_string.counter)])
