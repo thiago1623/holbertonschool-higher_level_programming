e688cb7b0199f8581a28da8beba423889e3e7b22
thiago1623 <1636@holbertonschool.com>
2020-10-06 19:49:36 -0500

    [update]--FILE--

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,3 +10,4 @@ def inherits_from(obj, a_class):
     """return the successful object"""
     if type(obj) != a_class:
         return(True if isinstance(obj, a_class) else False)
+    return False
