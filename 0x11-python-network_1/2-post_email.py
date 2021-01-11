ef4f4dd9cb8f5b3138b16c8510197d67557b6a87
thiago1623 <thiago.96t16@gmail.com>
2021-01-11 17:45:37 -0500

    [UPDATE]--FILES--(add new changes)

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,6 +7,7 @@ import sys
 
 
 if __name__ == "__main__":
+    """request"""
     data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
     req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Displays the body of a response from a url
+"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    """request"""
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
